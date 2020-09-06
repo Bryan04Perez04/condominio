f6f4040a471c8dd6ba299cc490255c36488b8a13
Perez <Bryan04Perez04@gmail.com>
2020-09-06 11:39:52 -0500

    Creación del BeanCondominio

---
+++ condominioWeb/src/condominio/modulos/condominio/view/controller/BeanCondominio.java
@@ -0,0 +1,17 @@
+package condominio.modulos.condominio.view.controller;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class BeanCondominio implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public BeanCondominio() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
