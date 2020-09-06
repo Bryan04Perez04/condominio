b5e98a066303670323a000c94bc0136b403e9472
Perez <Bryan04Perez04@gmail.com>
2020-09-06 11:15:40 -0500

    Elementos de interfaz del menú

---
+++ condominioEJB/ejbModule/condominio/modulos/condominio/model/ManagerCondominio.java
@@ -0,0 +1,20 @@
+package condominio.modulos.condominio.model;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerCondominio
+ */
+@Stateless
+@LocalBean
+public class ManagerCondominio {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerCondominio() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ condominioEJB/ejbModule/condominio/modulos/tesorero/model/ManagerTesorero.java
@@ -0,0 +1,20 @@
+package condominio.modulos.tesorero.model;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerTesorero
+ */
+@Stateless
+@LocalBean
+public class ManagerTesorero {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerTesorero() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
