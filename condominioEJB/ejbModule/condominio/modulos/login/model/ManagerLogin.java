401c99d04b888ceeeab25f4e2f4c5dfe40382d01
Perez <Bryan04Perez04@gmail.com>
2020-09-06 11:22:27 -0500

    Creación del ManagerLogin

---
+++ condominioEJB/ejbModule/condominio/modulos/login/model/ManagerLogin.java
@@ -0,0 +1,20 @@
+package condominio.modulos.login.model;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerLogin
+ */
+@Stateless
+@LocalBean
+public class ManagerLogin {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerLogin() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
