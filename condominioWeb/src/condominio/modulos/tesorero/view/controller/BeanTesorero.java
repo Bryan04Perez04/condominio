882b02036556c46bd8585102b290b73bc30ac473
Perez <Bryan04Perez04@gmail.com>
2020-09-06 11:39:11 -0500

    Creación del BeanTesorero

---
+++ condominioWeb/src/condominio/modulos/tesorero/view/controller/BeanTesorero.java
@@ -0,0 +1,17 @@
+package condominio.modulos.tesorero.view.controller;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class BeanTesorero implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public BeanTesorero() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
