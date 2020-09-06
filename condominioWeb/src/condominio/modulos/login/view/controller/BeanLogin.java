8aaa2b582ffef03563ddfccceddc74f1cfa76f7f
Perez <Bryan04Perez04@gmail.com>
2020-09-06 11:36:26 -0500

    Creación del BeanLogin

---
+++ condominioWeb/src/condominio/modulos/login/view/controller/BeanLogin.java
@@ -0,0 +1,17 @@
+package condominio.modulos.login.view.controller;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class BeanLogin implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public BeanLogin() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
